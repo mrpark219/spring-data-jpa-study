4610f676425e3de136583ccb23b4f7dde8560f45
mrpark219 <mrpark219@gmail.com>
2024-03-02 19:16:18 +0900

    🎉 실전! 스프링 데이터 JPA spring-data-jpa-study 시작

---
+++ src/main/java/springdatajpa/study/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package springdatajpa.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+	}
+
+}
